a123f8c691a39f23f36eff36a94c0d1ea600a26c
JoyJ <316351048@qq.com>
2026-03-05 12:50:16 +0800

    chore(scripts): added comments for cards c15286412, c1528054

--- c1528054.lua
+++ c1528054.lua
@@ -1,9 +1,17 @@
 --影法師トップハットヘア
+-- 效果：
+-- 效果怪兽2只
+-- 这个卡名的①③的效果1回合各能使用1次。
+-- ①：这张卡连接召唤的场合才能发动。把持有把自身作为怪兽特殊召唤效果的1张永续陷阱卡从卡组到自己场上盖放。这张卡在这个回合不能作为连接素材。
+-- ②：这张卡和怪兽进行战斗的场合，那2只不会被那次战斗破坏。
+-- ③：魔法与陷阱区域的卡在怪兽区域特殊召唤的场合，以对方场上1张魔法·陷阱卡为对象才能发动。那张卡破坏。
 local s,id,o=GetID()
+-- 为卡片添加连接召唤手续，需要2只满足效果怪兽类型的怪兽作为连接素材
 function s.initial_effect(c)
+	-- 添加连接召唤手续，要求连接素材必须是效果怪兽类型
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2)
 	c:EnableReviveLimit()
-	--
+	-- 效果①：这张卡连接召唤的场合才能发动。把持有把自身作为怪兽特殊召唤效果的1张永续陷阱卡从卡组到自己场上盖放。这张卡在这个回合不能作为连接素材。
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SSET)
@@ -15,7 +23,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.settg)
 	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
-	--
+	-- 效果②：这张卡和怪兽进行战斗的场合，那2只不会被那次战斗破坏。
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
@@ -24,7 +32,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.indtg)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--
+	-- 效果③：魔法与陷阱区域的卡在怪兽区域特殊召唤的场合，以对方场上1张魔法·陷阱卡为对象才能发动。那张卡破坏。
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DESTROY)
@@ -38,9 +46,11 @@ function s.initial_effect(c)
 	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
 end
+-- 效果①的发动条件：这张卡是通过连接召唤特殊召唤的
 function s.setcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
+-- 筛选满足条件的永续陷阱卡，包括其类型、是否可盖放以及是否具有基础属性值
 function s.filter(c)
 	return c:IsType(TYPE_TRAP) and c:IsType(TYPE_CONTINUOUS) and c:IsSSetable()
 		and (c:GetOriginalLevel()>0
@@ -49,21 +59,30 @@ function s.filter(c)
 		or c:GetBaseAttack()>0
 		or c:GetBaseDefense()>0)
 end
+-- 效果①的发动条件检查：判断场上是否有足够的魔法陷阱区域以及卡组中是否存在符合条件的永续陷阱卡
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 判断场上是否有足够的魔法陷阱区域
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		-- 判断卡组中是否存在符合条件的永续陷阱卡
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
 end
+-- 效果①的发动处理：选择并盖放一张符合条件的永续陷阱卡，并使自身在本回合不能作为连接素材
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	-- 判断场上是否有足够的魔法陷阱区域以进行盖放
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+		-- 提示玩家选择要盖放的卡
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		-- 从卡组中选择一张符合条件的永续陷阱卡
 		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 		local tc=g:GetFirst()
 		if tc then
+			-- 将选中的永续陷阱卡盖放到场上
 			Duel.SSet(tp,tc)
 		end
 	end
 	if c:IsRelateToEffect(e) then
+		-- 使自身在本回合不能作为连接素材
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -73,30 +92,42 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1,true)
 	end
 end
+-- 判断目标怪兽是否为自身或自身战斗对象
 function s.indtg(e,c)
 	local tc=e:GetHandler()
 	return c==tc or c==tc:GetBattleTarget()
 end
+-- 判断卡片是否从魔法陷阱区域被特殊召唤
 function s.cfilter(c,tp)
 	return c:IsPreviousLocation(LOCATION_SZONE)
 end
+-- 效果③的发动条件：不是自身被特殊召唤，且有从魔法陷阱区域特殊召唤的卡片
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return not eg:IsContains(c) and eg:IsExists(s.cfilter,1,nil)
 end
+-- 筛选魔法或陷阱类型的卡
 function s.desfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
+-- 效果③的发动处理：选择对方场上的魔法或陷阱卡进行破坏
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and s.desfilter(chkc) and chkc:IsControler(1-tp) end
+	-- 判断对方场上是否存在魔法或陷阱类型的卡
 	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_ONFIELD,1,nil) end
+	-- 提示玩家选择要破坏的卡
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	-- 选择对方场上的魔法或陷阱卡作为目标
 	local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	-- 设置连锁操作信息，确定要破坏的卡
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
+-- 效果③的发动处理：破坏选中的魔法或陷阱卡
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取连锁中的目标卡
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
+		-- 破坏目标卡
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
--- c15286412.lua
+++ c15286412.lua
@@ -1,6 +1,8 @@
 --極星宝グングニル
+-- 效果：
+-- 把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 function c15286412.initial_effect(c)
-	--Activate
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -11,30 +13,45 @@ function c15286412.initial_effect(c)
 	e1:SetOperation(c15286412.activate)
 	c:RegisterEffect(e1)
 end
+-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 function c15286412.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x42,0x4b) and c:IsAbleToRemoveAsCost()
 end
+-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 function c15286412.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	if chk==0 then return Duel.IsExistingMatchingCard(c15286412.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	local g=Duel.SelectMatchingCard(tp,c15286412.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	Duel.Remove(g,0,REASON_COST+REASON_TEMPORARY)
 	e:SetLabelObject(g:GetFirst())
 end
+-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 function c15286412.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc~=e:GetHandler() end
 	if chk==0 then
 		e:SetLabel(0)
+		-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 		return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler())
 	end
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
+-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 function c15286412.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	local tc=Duel.GetFirstTarget()
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 and e:GetLabel()==1 then
+		-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
@@ -44,15 +61,20 @@ function c15286412.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetLabel(2)
 		e1:SetLabelObject(e:GetLabelObject())
 		e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+		-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 		Duel.RegisterEffect(e1,tp)
 	end
 end
+-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 function c15286412.retcon(e,tp,eg,ep,ev,re,r,rp)
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	return Duel.GetTurnPlayer()==tp
 end
+-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 function c15286412.retop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()
 	ct=ct-1
 	e:SetLabel(ct)
+	-- 效果原文：把自己场上表侧表示存在的1只名字带有「极神」或者「极星」的怪兽从游戏中除外，选择场上存在的1张卡发动。选择的卡破坏。发动后第2次的自己的结束阶段时，为这个效果发动而从游戏中除外的怪兽表侧攻击表示回到场上。
 	if ct==0 then Duel.ReturnToField(e:GetLabelObject(),POS_FACEUP_ATTACK) end
 end
