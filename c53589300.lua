2cfcdb61fa1f3d0aca11ec7bc3a46670e6d12c33
JoyJ <316351048@qq.com>
2026-03-20 13:53:16 +0800

    chore(scripts): added comments for cards c53589300, c53610653, c5361647, c5361816, c53618197, c53618293, c53620899, c53623827, c53624265, c53639887, c53656677, c53666449, c53670497, c53678698, c5368615, c53693416, c53701259, c53701457, c5370235, c53714009

--- c53589300.lua
+++ c53589300.lua
@@ -1,18 +1,27 @@
 --創獄神ネルヴァ
+-- 效果：
+-- 「神艺」怪兽×3
+-- 自己对「创狱神 涅瓦」1回合只能有1次特殊召唤。这张卡用融合召唤以及以下方法才能从额外卡组特殊召唤。
+-- ●把种族不同的自己场上3只怪兽解放的场合可以守备表示特殊召唤。
+-- ①：只要场地区域有卡存在，这张卡不会被效果破坏。
+-- ②：1回合1次，自己把「神艺」怪兽的效果发动时才能发动。那个效果变成「对方场上的卡全部破坏」。
 local s,id,o=GetID()
+-- 初始化效果函数，设置卡片的特殊召唤限制、融合召唤手续、守备召唤条件、不被效果破坏和效果变更等效果
 function s.initial_effect(c)
 	c:SetSPSummonOnce(id)
+	-- 添加融合召唤手续，使用3个满足s.ffilter条件的怪兽作为融合素材
 	aux.AddFusionProcFunRep(c,s.ffilter,3,true)
 	c:EnableReviveLimit()
-	--special summon condition
+	-- 自己对「创狱神 涅瓦」1回合只能有1次特殊召唤。这张卡用融合召唤以及以下方法才能从额外卡组特殊召唤。
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e0:SetRange(LOCATION_EXTRA)
+	-- 设置该卡只能通过融合召唤特殊召唤
 	e0:SetValue(aux.fuslimit)
 	c:RegisterEffect(e0)
-	--spsummon
+	-- ●把种族不同的自己场上3只怪兽解放的场合可以守备表示特殊召唤。
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
@@ -23,7 +32,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.hsptg)
 	e1:SetOperation(s.hspop)
 	c:RegisterEffect(e1)
-	--indestructable
+	-- ①：只要场地区域有卡存在，这张卡不会被效果破坏。
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -32,9 +41,9 @@ function s.initial_effect(c)
 	e2:SetCondition(s.indcon)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--change effect
+	-- ②：1回合1次，自己把「神艺」怪兽的效果发动时才能发动。那个效果变成「对方场上的卡全部破坏」。
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,0))  --"效果变更"
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_CHAINING)
 	e3:SetRange(LOCATION_MZONE)
@@ -44,26 +53,35 @@ function s.initial_effect(c)
 	e3:SetOperation(s.chop)
 	c:RegisterEffect(e3)
 end
+-- 过滤函数，判断怪兽是否为「神艺」卡组
 function s.ffilter(c)
 	return c:IsFusionSetCard(0x1cd)
 end
+-- 过滤函数，判断怪兽是否可以被解放用于特殊召唤
 function s.hspfilter(c,tp,sc)
 	return c:IsControler(tp) and c:IsReleasable(REASON_SPSUMMON)
 		and c:IsCanBeFusionMaterial(sc,SUMMON_TYPE_SPECIAL)
 end
+-- 检查组是否满足解放条件，包括场地数量和种族不同
 function s.hspchk(g,tp,sc)
+	-- 检查是否有足够的场地用于特殊召唤
 	return Duel.GetLocationCountFromEx(tp,tp,g,sc)>0
 		and g:GetClassCount(Card.GetRace)==#g
 end
+-- 守备召唤条件函数，检查是否满足解放3只种族不同的怪兽的条件
 function s.hspcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
+	-- 获取满足守备召唤条件的怪兽组
 	local rg=Duel.GetMatchingGroup(s.hspfilter,tp,LOCATION_MZONE,0,nil,tp,e:GetHandler())
 	return rg:CheckSubGroup(s.hspchk,3,3,tp,e:GetHandler())
 end
+-- 守备召唤目标选择函数，选择3只满足条件的怪兽
 function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	-- 获取满足守备召唤条件的怪兽组
 	local rg=Duel.GetMatchingGroup(s.hspfilter,tp,LOCATION_MZONE,0,nil,tp,e:GetHandler())
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	-- 提示玩家选择要解放的怪兽
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)  --"请选择要解放的卡"
 	local sg=rg:SelectSubGroup(tp,s.hspchk,true,3,3,tp,e:GetHandler())
 	if sg then
 		sg:KeepAlive()
@@ -71,29 +89,42 @@ function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
 		return true
 	else return false end
 end
+-- 守备召唤处理函数，解放选中的怪兽
 function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
+	-- 实际执行解放操作
 	Duel.Release(g,REASON_SPSUMMON)
 	g:DeleteGroup()
 end
+-- 不被效果破坏的条件函数，判断场地区域是否有卡
 function s.indcon(e)
+	-- 判断场地区域是否有卡
 	return Duel.IsExistingMatchingCard(aux.TRUE,e:GetHandlerPlayer(),LOCATION_FZONE,LOCATION_FZONE,1,nil)
 end
+-- 效果变更的发动条件函数，判断是否为「神艺」怪兽的效果发动
 function s.chcon(e,tp,eg,ep,ev,re,r,rp)
 	local ec=re:GetHandler()
 	local b1=re:GetHandler():IsRelateToEffect(re) and ec:IsSetCard(0x1cd)
 	local b2=re:GetActivateLocation()==LOCATION_MZONE and not ec:IsLocation(LOCATION_MZONE) and ec:IsPreviousSetCard(0x1cd)
 	return rp==tp and re:IsActiveType(TYPE_MONSTER) and (b1 or b2)
 end
+-- 效果变更的目标选择函数，检查对方场上是否有卡
 function s.chtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 检查对方场上是否有卡
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,rp,0,LOCATION_ONFIELD,1,nil) end
 end
+-- 效果变更的处理函数，将连锁效果的目标改为对方场上所有卡
 function s.chop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Group.CreateGroup()
+	-- 将连锁效果的目标改为对方场上所有卡
 	Duel.ChangeTargetCard(ev,g)
+	-- 将连锁效果的处理函数改为s.repop函数
 	Duel.ChangeChainOperation(ev,s.repop)
 end
+-- 效果变更的处理函数，破坏对方场上所有卡
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取对方场上所有卡的组
 	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	-- 实际执行破坏操作
 	Duel.Destroy(sg,REASON_EFFECT)
 end
--- c53610653.lua
+++ c53610653.lua
@@ -1,6 +1,10 @@
 --バウンド・ワンド
+-- 效果：
+-- 魔法师族怪兽才能装备。
+-- ①：装备怪兽的攻击力上升装备怪兽的等级×100。
+-- ②：装备怪兽被对方破坏，这张卡被送去墓地的场合才能发动。那只怪兽从墓地往自己场上特殊召唤。
 function c53610653.initial_effect(c)
-	--Activate
+	-- ①：装备怪兽的攻击力上升装备怪兽的等级×100。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -9,22 +13,22 @@ function c53610653.initial_effect(c)
 	e1:SetTarget(c53610653.target)
 	e1:SetOperation(c53610653.operation)
 	c:RegisterEffect(e1)
-	--Atk up
+	-- ②：装备怪兽被对方破坏，这张卡被送去墓地的场合才能发动。那只怪兽从墓地往自己场上特殊召唤。
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetValue(c53610653.atkval)
 	c:RegisterEffect(e2)
-	--Equip limit
+	-- 魔法师族怪兽才能装备。
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_EQUIP_LIMIT)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e3:SetValue(c53610653.eqlimit)
 	c:RegisterEffect(e3)
-	--spsummon
+	-- ②：装备怪兽被对方破坏，这张卡被送去墓地的场合才能发动。那只怪兽从墓地往自己场上特殊召唤。
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(53610653,0))
+	e4:SetDescription(aux.Stringid(53610653,0))  --"特殊召唤"
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
@@ -34,43 +38,62 @@ function c53610653.initial_effect(c)
 	e4:SetOperation(c53610653.spop)
 	c:RegisterEffect(e4)
 end
+-- 限制装备对象为魔法师族怪兽。
 function c53610653.eqlimit(e,c)
 	return c:IsRace(RACE_SPELLCASTER)
 end
+-- 筛选场上正面表示的魔法师族怪兽作为装备目标。
 function c53610653.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER)
 end
+-- 设置装备效果的处理目标为场上正面表示的魔法师族怪兽。
 function c53610653.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c53610653.filter(chkc) end
+	-- 判断是否存在符合条件的装备目标。
 	if chk==0 then return Duel.IsExistingTarget(c53610653.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	-- 向玩家提示选择要装备的卡。
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)  --"请选择要装备的卡"
+	-- 选择一个场上正面表示的魔法师族怪兽作为装备对象。
 	Duel.SelectTarget(tp,c53610653.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	-- 设置装备效果的操作信息。
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
+-- 执行装备操作，将装备卡装备给目标怪兽。
 function c53610653.operation(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取当前连锁的装备对象。
 	local tc=Duel.GetFirstTarget()
 	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		-- 执行装备操作，将装备卡装备给目标怪兽。
 		Duel.Equip(tp,e:GetHandler(),tc)
 	end
 end
+-- 计算装备怪兽攻击力提升值，为等级×100。
 function c53610653.atkval(e,c)
 	return c:GetLevel()*100
 end
+-- 判断装备卡是否因失去装备对象且被破坏而送入墓地，且该怪兽在墓地且为对方破坏。
 function c53610653.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetHandler():GetPreviousEquipTarget()
 	return e:GetHandler():IsReason(REASON_LOST_TARGET) and ec and ec:IsReason(REASON_DESTROY)
 		and ec:IsLocation(LOCATION_GRAVE) and ec:GetReasonPlayer()==1-tp
 end
+-- 判断是否可以将装备卡的装备对象特殊召唤。
 function c53610653.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ec=e:GetHandler():GetPreviousEquipTarget()
+	-- 判断玩家场上是否有足够的召唤区域。
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and ec:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	-- 设置特殊召唤效果的目标卡为装备卡的装备对象。
 	Duel.SetTargetCard(ec)
+	-- 设置特殊召唤效果的操作信息。
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,ec,1,0,0)
 end
+-- 执行特殊召唤操作，将装备卡的装备对象从墓地特殊召唤到场上。
 function c53610653.spop(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取当前连锁的特殊召唤目标。
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
+		-- 将目标怪兽从墓地特殊召唤到场上。
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
