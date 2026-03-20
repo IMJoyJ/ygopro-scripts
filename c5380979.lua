45c3246b8f59fb47327d83fb689998eb2b861b74
JoyJ <316351048@qq.com>
2026-03-20 15:59:58 +0800

    chore(scripts): added comments for cards c5371656, c53724621, c5373478, c53742162, c53753697, c53754104, c5376159, c53765052, c53770666, c53776969, c53778229, c53782828, c53792930, c53797637, c53804307, c5380979, c53813120, c53819028, c53819808, c53828396

--- c5380979.lua
+++ c5380979.lua
@@ -1,6 +1,10 @@
 --ウェルカム・ラビュリンス
+-- 效果：
+-- 这个卡名的①②的效果1回合各能使用1次。
+-- ①：从卡组把1只「拉比林斯迷宫」怪兽特殊召唤。这张卡的发动后，直到下个回合的结束时自己不是恶魔族怪兽不能从卡组·额外卡组特殊召唤。
+-- ②：这张卡在墓地存在的状态，自己的通常陷阱卡的效果让怪兽从场上离开的场合才能发动。这张卡在自己场上盖放。这个效果在这张卡送去墓地的回合不能发动。
 function c5380979.initial_effect(c)
-	--Activate
+	-- ①：从卡组把1只「拉比林斯迷宫」怪兽特殊召唤。这张卡的发动后，直到下个回合的结束时自己不是恶魔族怪兽不能从卡组·额外卡组特殊召唤。
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(5380979,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -11,7 +15,7 @@ function c5380979.initial_effect(c)
 	e1:SetTarget(c5380979.target)
 	e1:SetOperation(c5380979.activate)
 	c:RegisterEffect(e1)
-	--set
+	-- ②：这张卡在墓地存在的状态，自己的通常陷阱卡的效果让怪兽从场上离开的场合才能发动。这张卡在自己场上盖放。这个效果在这张卡送去墓地的回合不能发动。
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(5380979,1))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O+CATEGORY_SSET)
@@ -24,26 +28,38 @@ function c5380979.initial_effect(c)
 	e2:SetOperation(c5380979.setop)
 	c:RegisterEffect(e2)
 end
+-- 过滤函数，用于筛选满足条件的「拉比林斯迷宫」怪兽
 function c5380979.spfilter(c,e,tp)
 	return c:IsSetCard(0x17e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
+-- 判断是否满足特殊召唤条件
 function c5380979.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 判断场上是否有空位
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		-- 判断卡组中是否存在满足条件的怪兽
 		and Duel.IsExistingMatchingCard(c5380979.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	-- 设置特殊召唤的操作信息
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+-- 发动效果时执行的处理函数
 function c5380979.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local res=0
+	-- 判断场上是否有空位
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		-- 提示玩家选择要特殊召唤的卡
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)  --"请选择要特殊召唤的卡"
+		-- 从卡组中选择满足条件的怪兽
 		local g=Duel.SelectMatchingCard(tp,c5380979.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 		if g:GetCount()>0 then
+			-- 将选中的怪兽特殊召唤
 			res=Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
+	-- 处理追加破坏效果
 	aux.LabrynthDestroyOp(e,tp,res)
 	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		-- 创建并注册不能特殊召唤恶魔族以外怪兽的效果
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -51,26 +67,35 @@ function c5380979.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetTargetRange(1,0)
 		e1:SetTarget(c5380979.splimit)
 		e1:SetReset(RESET_PHASE+PHASE_END,2)
+		-- 将效果注册到玩家
 		Duel.RegisterEffect(e1,tp)
 	end
 end
+-- 限制不能特殊召唤非恶魔族怪兽的效果函数
 function c5380979.splimit(e,c)
 	return not c:IsRace(RACE_FIEND) and c:IsLocation(LOCATION_DECK+LOCATION_EXTRA)
 end
+-- 判断怪兽是否从场上离开的过滤函数
 function c5380979.cfilter(c)
 	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_EFFECT)
 end
+-- 判断是否满足盖放条件
 function c5380979.setcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c5380979.cfilter,1,nil) and not eg:IsContains(e:GetHandler()) and rp==tp
+		-- 判断发动的陷阱卡是否为通常陷阱卡
 		and re:IsActiveType(TYPE_TRAP) and re:GetHandler():GetOriginalType()==TYPE_TRAP and aux.exccon(e)
 end
+-- 设置盖放操作信息
 function c5380979.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsSSetable() end
+	-- 设置盖放操作信息
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
 end
+-- 执行盖放操作
 function c5380979.setop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
+		-- 将卡片盖放
 		Duel.SSet(tp,c)
 	end
 end
--- c53828396.lua
+++ c53828396.lua
@@ -1,8 +1,10 @@
 --瞬着ボマー
+-- 效果：
+-- 里侧守备表示的这张卡被对方怪兽攻击的场合，不进行伤害计算让这张卡变成攻击怪兽的装备卡。下次的对方回合的准备阶段时，那只装备怪兽破坏。
 function c53828396.initial_effect(c)
-	--equip
+	-- 里侧守备表示的这张卡被对方怪兽攻击的场合，不进行伤害计算让这张卡变成攻击怪兽的装备卡。
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(53828396,0))
+	e1:SetDescription(aux.Stringid(53828396,0))  --"变成装备卡"
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_BATTLE_CONFIRM)
@@ -10,18 +12,26 @@ function c53828396.initial_effect(c)
 	e1:SetOperation(c53828396.eqop)
 	c:RegisterEffect(e1)
 end
+-- 判断攻击目标是否为该卡且该卡处于里侧守备表示
 function c53828396.eqcon(e,tp,eg,ep,ev,re,r,rp)
+	-- 攻击目标为该卡且该卡处于里侧守备表示
 	return Duel.GetAttackTarget()==e:GetHandler() and e:GetHandler():GetBattlePosition()==POS_FACEDOWN_DEFENSE
 end
+-- 处理装备效果：获取攻击怪兽，检查是否满足装备条件，若不满足则破坏该卡，否则进行装备操作
 function c53828396.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	-- 获取此次战斗的攻击怪兽
 	local tc=Duel.GetAttacker()
 	if not tc:IsRelateToBattle() or not c:IsRelateToBattle() then return end
+	-- 判断场上魔陷区是否还有空位或攻击怪兽是否里侧表示
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or tc:IsFacedown() then
+		-- 若场上魔陷区无空位或攻击怪兽为里侧表示，则将该卡破坏
 		Duel.Destroy(c,REASON_EFFECT)
 		return
 	end
+	-- 将该卡装备给攻击怪兽
 	Duel.Equip(tp,c,tc)
+	-- 为装备怪兽设置装备限制效果，防止被其他卡装备
 	local e1=Effect.CreateEffect(tc)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_EQUIP_LIMIT)
@@ -29,9 +39,9 @@ function c53828396.eqop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	e1:SetValue(c53828396.eqlimit)
 	c:RegisterEffect(e1)
-	--destroy&damage
+	-- 下次的对方回合的准备阶段时，那只装备怪兽破坏。
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(53828396,1))
+	e2:SetDescription(aux.Stringid(53828396,1))  --"破坏"
 	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
@@ -43,25 +53,33 @@ function c53828396.eqop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,2)
 	c:RegisterEffect(e2)
 end
+-- 装备限制效果的判断函数，确保只能装备给拥有者
 function c53828396.eqlimit(e,c)
 	return e:GetOwner()==c
 end
+-- 判断当前回合玩家是否为非发动者
 function c53828396.descon(e,tp,eg,ep,ev,re,r,rp)
+	-- 当前回合玩家不是发动者
 	return Duel.GetTurnPlayer()~=tp
 end
+-- 设置破坏效果的目标信息，确定要破坏的装备怪兽
 function c53828396.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local ec=e:GetHandler():GetEquipTarget()
 	ec:CreateEffectRelation(e)
 	e:SetLabelObject(ec)
+	-- 设置连锁操作信息，指定要破坏的装备怪兽
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,ec,1,0,0)
 end
+-- 执行破坏效果：若装备怪兽存在且表侧表示，则破坏该怪兽，否则破坏装备卡
 function c53828396.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	local ec=e:GetLabelObject()
 	if ec:IsRelateToEffect(e) and ec:IsFaceup() then
+		-- 若破坏装备怪兽成功，则不执行后续破坏操作
 		if Duel.Destroy(ec,REASON_EFFECT)~=0 then
+		-- 若破坏装备怪兽失败，则将装备卡破坏
 		else Duel.Destroy(c,REASON_EFFECT) end
 	end
 end
