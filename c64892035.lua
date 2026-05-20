65704c80f8f9d2473bf430f1f45801f700731634
JoyJ <316351048@qq.com>
2026-05-20 18:29:17 +0800

    chore(scripts): added comments for cards c6483224, c64867422, c64880894, c64881644, c64892035, c64898834, c64910482, c64911387, c64926005, c64927055, c6494106, c64952266, c64961254, c64964750, c64966519, c64973287, c64973456, c64977888, c6498706, c64990807

--- c64892035.lua
+++ c64892035.lua
@@ -1,13 +1,17 @@
 --遅すぎたオーク
+-- 效果：
+-- 这张卡在召唤的回合不能攻击。
 function c64892035.initial_effect(c)
-	--cannot attack
+	-- 这张卡在召唤的回合不能攻击。
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetOperation(c64892035.atklimit)
 	c:RegisterEffect(e1)
 end
+-- 通常召唤成功时，给自身施加回合结束前不能攻击的限制
 function c64892035.atklimit(e,tp,eg,ep,ev,re,r,rp)
+	-- 不能攻击
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)
