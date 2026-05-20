68cb9d83bc9b481657f40471d60bb6f45550570a
JoyJ <316351048@qq.com>
2026-05-21 04:11:47 +0800

    chore(scripts): added comments for cards c80161395, c80163754, c80168720, c80170678, c80181649, c80186010, c80190753, c80193355, c80196387, c80204957, c80208158, c80208225, c80208323, c80230510, c80233946, c80237445, c80244114, c80250185, c80250319, c80254726

--- c80168720.lua
+++ c80168720.lua
@@ -1,6 +1,8 @@
 --闇の訪れ
+-- 效果：
+-- 丢弃2张手卡。选择表侧表示的1只怪兽，变成里侧守备表示。
 function c80168720.initial_effect(c)
-	--Activate
+	-- 丢弃2张手卡。选择表侧表示的1只怪兽，变成里侧守备表示。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_POSITION+CATEGORY_MSET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -11,23 +13,35 @@ function c80168720.initial_effect(c)
 	e1:SetOperation(c80168720.activate)
 	c:RegisterEffect(e1)
 end
+-- 发动代价（Cost）：丢弃2张手卡
 function c80168720.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 在发动检查阶段，确认手牌中是否存在至少2张可以丢弃的卡（排除这张卡自身）
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,e:GetHandler()) end
+	-- 玩家选择并丢弃2张手牌作为发动代价
 	Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
 end
+-- 过滤函数：筛选场上表侧表示且可以转为里侧表示的怪兽
 function c80168720.filter(c)
 	return c:IsFaceup() and c:IsCanTurnSet()
 end
+-- 效果的目标选择（Target）处理
 function c80168720.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c80168720.filter(chkc) end
+	-- 在发动检查阶段，确认双方场上是否存在至少1只满足过滤条件的怪兽
 	if chk==0 then return Duel.IsExistingTarget(c80168720.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	-- 发送系统提示信息：请选择表侧表示的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)  --"请选择表侧表示的卡"
+	-- 选择双方场上1只满足过滤条件的怪兽作为效果对象
 	local g=Duel.SelectTarget(tp,c80168720.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	-- 设置操作信息：此效果包含改变表示形式的操作，涉及1个对象
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
+-- 效果处理（Operation）阶段
 function c80168720.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取发动时选择的对象怪兽
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		-- 将目标怪兽的表示形式变更为里侧守备表示
 		Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)
 	end
 end
--- c80233946.lua
+++ c80233946.lua
@@ -1,6 +1,8 @@
 --ゴラ・タートル
+-- 效果：
+-- 只要这张卡在场上表侧表示存在，攻击力1900以上的怪兽不能进行攻击宣言。
 function c80233946.initial_effect(c)
-	--cannot attack
+	-- 只要这张卡在场上表侧表示存在，攻击力1900以上的怪兽不能进行攻击宣言。
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
@@ -9,6 +11,7 @@ function c80233946.initial_effect(c)
 	e1:SetTarget(c80233946.atktarget)
 	c:RegisterEffect(e1)
 end
+-- 过滤出场上攻击力在1900以上的怪兽作为限制攻击宣言的对象
 function c80233946.atktarget(e,c)
 	return c:IsAttackAbove(1900)
 end
