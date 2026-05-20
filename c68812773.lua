b68067b86ba4a1ccd452ff60c04fde8a73fae56c
JoyJ <316351048@qq.com>
2026-05-20 20:54:03 +0800

    chore(scripts): added comments for cards c68774379, c68779682, c68786330, c68809475, c68810435, c68811206, c68812773, c68815132, c68815401, c68819554, c68823957, c68829754, c68831625, c68833958, c68836428, c68860936, c68875140, c68881649, c68897338, c6890729

--- c68812773.lua
+++ c68812773.lua
@@ -1,6 +1,9 @@
 --オボミ
+-- 效果：
+-- ①：这张卡召唤·反转时，以自己墓地1只「轨道 7」为对象才能发动。那只怪兽表侧攻击表示或者里侧守备表示特殊召唤。
+-- ②：1回合1次，把自己场上的机械族怪兽任意数量解放才能发动。把解放数量的「光子」怪兽或者「银河」怪兽从手卡特殊召唤。
 function c68812773.initial_effect(c)
-	--summon,flip
+	-- ①：这张卡召唤·反转时，以自己墓地1只「轨道 7」为对象才能发动。那只怪兽表侧攻击表示或者里侧守备表示特殊召唤。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_MSET)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -12,9 +15,9 @@ function c68812773.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_FLIP)
 	c:RegisterEffect(e2)
-	--
+	-- ②：1回合1次，把自己场上的机械族怪兽任意数量解放才能发动。把解放数量的「光子」怪兽或者「银河」怪兽从手卡特殊召唤。
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(68812773,0))
+	e3:SetDescription(aux.Stringid(68812773,0))  --"特殊召唤"
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
@@ -24,55 +27,84 @@ function c68812773.initial_effect(c)
 	e3:SetOperation(c68812773.spop2)
 	c:RegisterEffect(e3)
 end
+-- 过滤条件：自己墓地中可以表侧攻击表示或里侧守备表示特殊召唤的「轨道 7」
 function c68812773.filter(c,e,tp)
 	return c:IsCode(71071546) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK+POS_FACEDOWN_DEFENSE)
 end
+-- 效果①的靶向/发动准备阶段：检查怪兽区域空位，并选择自己墓地1只「轨道 7」作为效果对象
 function c68812773.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c68812773.filter(chkc,e,tp) end
+	-- 在发动准备阶段，检查自己场上是否有可用的怪兽区域空格
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		-- 检查自己墓地是否存在至少1只满足条件的「轨道 7」作为可选对象
 		and Duel.IsExistingTarget(c68812773.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	-- 向玩家发送提示信息：请选择要特殊召唤的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)  --"请选择要特殊召唤的卡"
+	-- 让玩家选择自己墓地1只满足条件的「轨道 7」作为效果对象
 	local g=Duel.SelectTarget(tp,c68812773.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	-- 设置当前连锁的操作信息：包含特殊召唤分类，操作对象为选择的怪兽，数量为1
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
+-- 效果①的处理阶段：将作为对象的怪兽表侧攻击表示或者里侧守备表示特殊召唤
 function c68812773.spop(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取当前连锁中被选择为效果对象的怪兽
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
+		-- 将对象怪兽以表侧攻击表示或里侧守备表示特殊召唤，若特殊召唤成功且为里侧表示则进行后续处理
 		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK+POS_FACEDOWN_DEFENSE)~=0 and tc:IsFacedown() then
+			-- 让对方玩家确认里侧守备表示特殊召唤的怪兽
 			Duel.ConfirmCards(1-tp,tc)
 		end
 	end
 end
+-- 过滤条件：手卡中可以特殊召唤的「光子」怪兽或「银河」怪兽
 function c68812773.spfilter(c,e,tp)
 	return c:IsSetCard(0x7b,0x55) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
+-- 过滤条件：自己场上可解放的机械族怪兽（需考虑解放后是否能腾出怪兽区域空格）
 function c68812773.cfilter(c,ft,tp)
 	return c:IsRace(RACE_MACHINE)
 		and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5)) and (c:IsControler(tp) or c:IsFaceup())
 end
+-- 效果②的发动代价阶段：解放自己场上任意数量的机械族怪兽，并记录解放的数量
 function c68812773.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 获取自己场上可用的怪兽区域空格数
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	-- 在发动准备阶段，检查自己场上是否存在至少1只可解放的机械族怪兽
 	if chk==0 then return ft>-1 and Duel.CheckReleaseGroup(tp,c68812773.cfilter,1,nil,ft,tp) end
+	-- 计算手卡中满足特殊召唤条件的「光子」或「银河」怪兽的最大数量
 	local ct=Duel.GetMatchingGroupCount(c68812773.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+	-- 检测【青眼精灵龙】(59822133)的怪兽效果是否生效中。禁止双方同时特殊召唤2只以上怪兽
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ct=1 end
+	-- 让玩家选择1张到最大数量张自己场上的机械族怪兽进行解放
 	local rg=Duel.SelectReleaseGroup(tp,c68812773.cfilter,1,ct,nil,ft,tp)
+	-- 解放选择的怪兽作为发动代价，并获取实际解放的数量
 	ct=Duel.Release(rg,REASON_COST)
 	e:SetLabel(ct)
 end
+-- 效果②的靶向/发动准备阶段：检查手卡中是否存在可特殊召唤的怪兽，并设置特殊召唤的操作信息
 function c68812773.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 在发动准备阶段，检查手卡中是否存在至少1只满足特殊召唤条件的「光子」或「银河」怪兽
 	if chk==0 then return Duel.IsExistingMatchingCard(c68812773.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	local ct=e:GetLabel()
+	-- 设置当前连锁的操作信息：包含特殊召唤分类，数量为解放的怪兽数量
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ct,0,0)
 end
+-- 效果②的处理阶段：从手卡选择与解放数量相同的「光子」怪兽或者「银河」怪兽表侧表示特殊召唤
 function c68812773.spop2(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取当前自己场上可用的怪兽区域空格数
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<=0 then return end
+	-- 检测【青眼精灵龙】(59822133)的怪兽效果是否生效中。禁止双方同时特殊召唤2只以上怪兽
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	local ct=e:GetLabel()
 	if ft<ct then ct=ft end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	-- 向玩家发送提示信息：请选择要特殊召唤的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)  --"请选择要特殊召唤的卡"
+	-- 让玩家从手卡选择与解放数量相同（且不超过可用空格数）的「光子」或「银河」怪兽
 	local dg=Duel.SelectMatchingCard(tp,c68812773.spfilter,tp,LOCATION_HAND,0,ct,ct,nil,e,tp)
 	if dg:GetCount()>0 then
+		-- 将选择的怪兽表侧表示特殊召唤到自己场上
 		Duel.SpecialSummon(dg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
--- c6890729.lua
+++ c6890729.lua
@@ -1,7 +1,11 @@
 --ボンディング－DHO
+-- 效果：
+-- ①：从自己的手卡·墓地让「氘素龙」「氢素龙」「氧素龙」各1只回到卡组才能发动。从自己的手卡·墓地选1只「水龙-团簇」特殊召唤。
+-- ②：把墓地的这张卡除外才能发动。从自己的卡组·墓地选1只「水龙」或者「水龙-团簇」加入手卡。
 function c6890729.initial_effect(c)
+	-- 注册本卡效果中涉及到的相关卡片密码列表（氘素龙、氢素龙、氧素龙、水龙-团簇、水龙）
 	aux.AddCodeList(c,43017476,22587018,58071123,6022371,85066822)
-	--activate
+	-- ①：从自己的手卡·墓地让「氘素龙」「氢素龙」「氧素龙」各1只回到卡组才能发动。从自己的手卡·墓地选1只「水龙-团簇」特殊召唤。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -10,59 +14,87 @@ function c6890729.initial_effect(c)
 	e1:SetTarget(c6890729.target)
 	e1:SetOperation(c6890729.activate)
 	c:RegisterEffect(e1)
-	--search
+	-- ②：把墓地的这张卡除外才能发动。从自己的卡组·墓地选1只「水龙」或者「水龙-团簇」加入手卡。
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
+	-- 设置把墓地的这张卡除外作为发动代价
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(c6890729.thtg)
 	e2:SetOperation(c6890729.thop)
 	c:RegisterEffect(e2)
 end
+-- 创建用于检查是否包含「氘素龙」「氢素龙」「氧素龙」各1张的条件检查函数数组
 c6890729.spchecks=aux.CreateChecks(Card.IsCode,{43017476,22587018,58071123})
+-- 过滤手卡·墓地中可以作为代价回到卡组的「氘素龙」「氢素龙」「氧素龙」
 function c6890729.spcostfilter(c)
 	return c:IsAbleToDeckAsCost() and c:IsCode(43017476,22587018,58071123)
 end
+-- ①号效果的发动代价与可行性检查函数
 function c6890729.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 获取自己手卡·墓地中满足条件的「氘素龙」「氢素龙」「氧素龙」卡片组
 	local g=Duel.GetMatchingGroup(c6890729.spcostfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil)
+	-- 检查自己场上是否有可用的怪兽区域空格
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and g:CheckSubGroupEach(c6890729.spchecks) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	-- 提示玩家选择要返回卡组的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)  --"请选择要返回卡组的卡"
 	local sg=g:SelectSubGroupEach(tp,c6890729.spchecks)
+	-- 给对方玩家确认选中的卡片
 	Duel.ConfirmCards(1-tp,sg)
+	-- 将选中的卡片作为代价送回卡组并洗牌
 	Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_COST)
 end
+-- 过滤手卡·墓地中可以特殊召唤的「水龙-团簇」
 function c6890729.filter(c,e,tp)
 	return c:IsCode(6022371) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
+-- ①号效果的发动目标检查与操作信息设置
 function c6890729.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 检查自己场上是否有可用的怪兽区域空格
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		-- 检查手卡·墓地是否存在至少1只可以特殊召唤的「水龙-团簇」
 		and Duel.IsExistingMatchingCard(c6890729.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	-- 设置特殊召唤的操作信息（从手卡·墓地特殊召唤1只怪兽）
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
+-- ①号效果的效果处理（特殊召唤「水龙-团簇」）
 function c6890729.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- 检查怪兽区域空格，若无可用空格则不处理
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	-- 提示玩家选择要特殊召唤的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)  --"请选择要特殊召唤的卡"
+	-- 过滤并选择1只手卡·墓地中不受王家长眠之谷影响的「水龙-团簇」
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c6890729.filter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
+		-- 将选中的怪兽无视召唤条件和苏生限制以表侧表示特殊召唤
 		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
 		g:GetFirst():CompleteProcedure()
 	end
 end
+-- 过滤卡组·墓地中可以加入手卡的「水龙」或「水龙-团簇」
 function c6890729.thfilter(c)
 	return c:IsCode(6022371,85066822) and c:IsAbleToHand()
 end
+-- ②号效果的发动目标检查与操作信息设置
 function c6890729.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 检查卡组·墓地是否存在至少1只可以加入手卡的「水龙」或「水龙-团簇」
 	if chk==0 then return Duel.IsExistingMatchingCard(c6890729.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	-- 设置加入手卡的操作信息（从卡组·墓地将1张卡加入手卡）
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
+-- ②号效果的效果处理（检索或回收「水龙」或「水龙-团簇」）
 function c6890729.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	-- 提示玩家选择要加入手牌的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)  --"请选择要加入手牌的卡"
+	-- 过滤并选择1张卡组·墓地中不受王家长眠之谷影响的「水龙」或「水龙-团簇」
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c6890729.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
+		-- 将选中的卡片加入手卡
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		-- 给对方玩家确认加入手卡的卡片
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
