e4f8e4abb42067e04b527c03061eb13359aad5b1
JoyJ <316351048@qq.com>
2026-05-21 00:38:57 +0800

    chore(scripts): added comments for cards c74157028, c74163487, c74168099, c74169516, c74191528, c74191942, c74202664, c74203495, c74210057, c74213995, c74218258, c74270067, c74271714, c74289646, c74294676, c74298287, c74311226, c74329404, c74335036, c7436169

--- c74213995.lua
+++ c74213995.lua
@@ -1,8 +1,16 @@
 --バーバリアン・エコーズ
+-- 效果：
+-- 这个卡名的卡在1回合只能发动1张。
+-- ①：作为这张卡的发动时的效果处理，可以从卡组把1只战士族·地属性·5星怪兽加入手卡。那个场合，再选自己1张手卡丢弃。
+-- ②：1回合1次，场上的怪兽回到手卡的场合，可以从以下效果选择1个发动（伤害步骤也能发动）。
+-- ●从卡组把1张「野蛮人之怒」或「野蛮人之吼」加入手卡。
+-- ●场上1只表侧表示怪兽变成里侧守备表示。
 local s,id,o=GetID()
+-- 注册卡片效果的初始化函数。
 function s.initial_effect(c)
+	-- 将「野蛮人之怒」与「野蛮人之吼」记录在此卡的关联卡片列表中。
 	aux.AddCodeList(c,42233477,78621186)
-	--
+	-- 这个卡名的卡在1回合只能发动1张。①：作为这张卡的发动时的效果处理，可以从卡组把1只战士族·地属性·5星怪兽加入手卡。那个场合，再选自己1张手卡丢弃。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_HANDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -10,9 +18,9 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--
+	-- ②：1回合1次，场上的怪兽回到手卡的场合，可以从以下效果选择1个发动（伤害步骤也能发动）。
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,1))  --"检索或怪兽变成里侧"
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_POSITION+CATEGORY_MSET)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_TO_HAND)
@@ -24,66 +32,94 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
+-- 过滤条件：卡组中等级5的战士族·地属性且能加入手牌的怪兽。
 function s.filter(c)
 	return c:IsLevel(5) and c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToHand()
 end
+-- 魔法卡发动时的效果处理函数。
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取自己卡组中所有满足条件的战士族·地属性·5星怪兽。
 	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
-	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	-- 如果卡组中存在满足条件的怪兽，询问玩家是否发动检索效果。
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then  --"是否从卡组把1只战士族·地属性·5星怪兽加入手卡？"
+		-- 提示玩家选择要加入手牌的卡。
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)  --"请选择要加入手牌的卡"
 		local sg=g:Select(tp,1,1,nil)
+		-- 将选中的怪兽加入手牌。
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		-- 向对方玩家确认加入手牌的卡。
 		Duel.ConfirmCards(1-tp,sg)
+		-- 中断当前效果处理，使后续的丢弃手牌处理不与检索同时进行。
 		Duel.BreakEffect()
+		-- 玩家选择自己1张手牌丢弃。
 		Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
 	end
 end
+-- 过滤条件：原本在怪兽区域的卡。
 function s.cfilter(c)
 	return c:IsPreviousLocation(LOCATION_MZONE)
 end
+-- 触发条件：场上的怪兽回到手牌的场合。
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil)
 end
+-- 过滤条件：卡组中卡名为「野蛮人之怒」或「野蛮人之吼」且能加入手牌的卡。
 function s.thfilter(c)
 	return c:IsCode(42233477,78621186) and c:IsAbleToHand()
 end
+-- 过滤条件：场上表侧表示且可以变成里侧表示的怪兽。
 function s.tsfilter(c)
 	return c:IsFaceup() and c:IsCanTurnSet()
 end
+-- 效果②的发动准备与分支选择处理。
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
+	-- 检查自己卡组中是否存在「野蛮人之怒」或「野蛮人之吼」。
 	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+	-- 检查场上是否存在可以变成里侧守备表示的表侧表示怪兽。
 	local b2=Duel.IsExistingMatchingCard(s.tsfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 	if chk==0 then return b1 or b2 end
+	-- 让玩家从可用的分支效果中选择一个。
 	local op=aux.SelectFromOptions(tp,
-			{b1,aux.Stringid(id,3)},
-			{b2,aux.Stringid(id,4)})
+			{b1,aux.Stringid(id,3)},  --"从卡组把1张「野蛮人之怒」或者「野蛮人之吼」加入手卡"
+			{b2,aux.Stringid(id,4)})  --"场上1只表侧表示怪兽变成里侧守备表示"
 	e:SetLabel(op)
 	local g=nil
 	if op==1 then
+		-- 设置效果分类为检索卡组，并预估操作数量为1张。
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	end
 	if op==2 then
+		-- 设置效果分类为改变表示形式，并预估操作数量为1张。
 		Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,tp,LOCATION_MZONE)
 		e:SetCategory(CATEGORY_POSITION+CATEGORY_MSET)
 	end
 end
+-- 效果②的分支效果执行函数。
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local op=e:GetLabel()
 	if op==1 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		-- 提示玩家选择要加入手牌的卡。
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)  --"请选择要加入手牌的卡"
+		-- 玩家从卡组中选择1张「野蛮人之怒」或「野蛮人之吼」。
 		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if g:GetCount()>0 then
+			-- 将选中的卡加入手牌。
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			-- 向对方玩家确认加入手牌的卡。
 			Duel.ConfirmCards(1-tp,g)
 		end
 	end
 	if op==2 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+		-- 提示玩家选择要改变表示形式的怪兽。
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)  --"请选择要改变表示形式的怪兽"
+		-- 玩家选择场上1只表侧表示的怪兽。
 		local g=Duel.SelectMatchingCard(tp,s.tsfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		if g:GetCount()>0 then
+			-- 选中该怪兽并显示选择动画。
 			Duel.HintSelection(g)
+			-- 将选中的怪兽变成里侧守备表示。
 			Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
 		end
 	end
--- c74271714.lua
+++ c74271714.lua
@@ -1,9 +1,13 @@
 --フィッシュアンドビッズ
+-- 效果：
+-- ①：自己主要阶段1开始时，把1张手卡除外才能发动。对方可以把2张手卡除外。没除外的场合，自己让以下效果适用。
+-- ●从卡组选2只鱼族怪兽全部送去墓地或全部除外。这个回合，自己不是鱼族怪兽不能特殊召唤。
 local s,id,o=GetID()
+-- 创建并注册卡片发动时的效果
 function s.initial_effect(c)
-	--Activate
+	-- ①：自己主要阶段1开始时，把1张手卡除外才能发动。对方可以把2张手卡除外。没除外的场合，自己让以下效果适用。●从卡组选2只鱼族怪兽全部送去墓地或全部除外。这个回合，自己不是鱼族怪兽不能特殊召唤。
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(id,0))  --"发动"
 	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_REMOVE+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -13,46 +17,70 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+-- 定义发动代价（Cost）函数：把1张手卡除外
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
+	-- 在发动检查阶段，确认自己手卡中是否存在除这张卡以外可以除外的卡
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_HAND,0,1,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	-- 提示玩家选择要除外的卡片
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)  --"请选择要除外的卡"
+	-- 让玩家选择1张手卡作为发动代价
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemoveAsCost,tp,LOCATION_HAND,0,1,1,c)
+	-- 将选择的手卡除外
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
+-- 定义发动条件函数：自己主要阶段1开始时
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	-- 检查当前是否为自己主要阶段1的开始时（尚未进行任何行动）
 	return Duel.GetCurrentPhase()==PHASE_MAIN1 and not Duel.CheckPhaseActivity()
 end
+-- 过滤卡组中的鱼族怪兽
 function s.filter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_FISH)
 end
+-- 检查选出的2张卡是否能全部送去墓地或全部除外
 function s.gcheck(g)
 	return #g==2 and (g:FilterCount(Card.IsAbleToGrave,nil)==2 or g:FilterCount(Card.IsAbleToRemove,nil)==2)
 end
+-- 定义效果的目标处理（Target）函数
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 获取卡组中所有的鱼族怪兽
 	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
 	if chk==0 then return dg:CheckSubGroup(s.gcheck,2,2) end
+	-- 设置将2张卡送去墓地的操作信息
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,LOCATION_HAND+LOCATION_DECK)
+	-- 设置将2张卡除外的操作信息
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,2,tp,LOCATION_HAND+LOCATION_DECK)
 end
+-- 定义效果的运行处理（Operation）函数
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- 获取对方手卡中可以除外的卡片
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil,1-tp)
-	if #g>1 and Duel.SelectYesNo(1-tp,aux.Stringid(id,1)) then
-		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_REMOVE)
+	-- 如果对方手卡有2张以上，询问对方是否选择除外2张手卡
+	if #g>1 and Duel.SelectYesNo(1-tp,aux.Stringid(id,1)) then  --"是否除外手卡？"
+		-- 提示对方玩家选择要除外的卡
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_REMOVE)  --"请选择要除外的卡"
 		local tc=g:Select(1-tp,2,2,nil)
+		-- 将对方选择的2张手卡除外
 		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 		return
 	end
+	-- 获取自己卡组中所有的鱼族怪兽
 	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
 	if dg:CheckSubGroup(s.gcheck,2,2) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+		-- 提示玩家选择要操作的卡
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)  --"请选择要操作的卡"
 		local sg1=dg:SelectSubGroup(tp,s.gcheck,false,2,2)
+		-- 判断选中的卡是否能送去墓地，并让玩家选择是送去墓地还是除外
 		if sg1:GetFirst():IsAbleToGrave() and (not sg1:GetFirst():IsAbleToRemove() or Duel.SelectOption(tp,1191,1192)==0) then
+			-- 将选中的2只鱼族怪兽全部送去墓地
 			Duel.SendtoGrave(sg1,REASON_EFFECT)
 		elseif sg1:GetFirst():IsAbleToRemove() then
+			-- 将选中的2只鱼族怪兽全部除外
 			Duel.Remove(sg1,POS_FACEUP,REASON_EFFECT)
 		end
 	end
+	-- 这个回合，自己不是鱼族怪兽不能特殊召唤。
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
@@ -60,8 +88,10 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
+	-- 向玩家注册不能特殊召唤鱼族以外怪兽的限制效果
 	Duel.RegisterEffect(e1,tp)
 end
+-- 限制只能特殊召唤鱼族怪兽
 function s.splimit(e,c)
 	return not c:IsRace(RACE_FISH)
 end
