d9a1c6f37e3c14abcfec48273798812776353cc1
JoyJ <316351048@qq.com>
2026-05-21 04:50:23 +0800

    chore(scripts): added comments for cards c81332143, c81336148, c81344070, c81344637, c81354330, c81380218, c81383947, c81384993, c81385346, c81418467, c81426505, c81434470, c81439173, c81443745, c81470373, c81471108, c81476402, c81480460, c81481818, c8148322

--- c81384993.lua
+++ c81384993.lua
@@ -1,6 +1,8 @@
 --Sin Cross
+-- 效果：
+-- ①：以自己墓地1只「罪」怪兽为对象才能发动。那只怪兽无视召唤条件特殊召唤。这个效果特殊召唤的怪兽的效果无效化，结束阶段除外。
 function c81384993.initial_effect(c)
-	--Activate
+	-- ①：以自己墓地1只「罪」怪兽为对象才能发动。那只怪兽无视召唤条件特殊召唤。这个效果特殊召唤的怪兽的效果无效化，结束阶段除外。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -10,27 +12,40 @@ function c81384993.initial_effect(c)
 	e1:SetOperation(c81384993.activate)
 	c:RegisterEffect(e1)
 end
+-- 过滤自己墓地中可以特殊召唤的「罪」怪兽
 function c81384993.filter(c,e,tp)
 	return c:IsSetCard(0x23) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
+-- 效果①的发动目标确认与合法性检测
 function c81384993.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c81384993.filter(chkc,e,tp) end
+	-- 检查自己场上是否有空余的怪兽区域
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		-- 检查自己墓地是否存在符合条件的「罪」怪兽
 		and Duel.IsExistingTarget(c81384993.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	-- 提示玩家选择要特殊召唤的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)  --"请选择要特殊召唤的卡"
+	-- 选择自己墓地1只「罪」怪兽作为效果对象
 	local g=Duel.SelectTarget(tp,c81384993.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	-- 设置特殊召唤的操作信息
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
+-- 效果①的处理逻辑（特殊召唤、无效化效果、注册结束阶段除外的效果）
 function c81384993.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- 检查自己场上是否有空余的怪兽区域，若无则不处理
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	-- 获取作为效果对象的怪兽
 	local tc=Duel.GetFirstTarget()
+	-- 若对象怪兽仍符合条件，则无视召唤条件将其以表侧表示特殊召唤
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP) then
 		local c=e:GetHandler()
+		-- 这个效果特殊召唤的怪兽的效果无效化
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
+		-- 这个效果特殊召唤的怪兽的效果无效化
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
@@ -39,6 +54,7 @@ function c81384993.activate(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e2)
 		local fid=c:GetFieldID()
 		tc:RegisterFlagEffect(81384993,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+		-- 结束阶段除外。
 		local e3=Effect.CreateEffect(c)
 		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e3:SetCode(EVENT_PHASE+PHASE_END)
@@ -48,10 +64,13 @@ function c81384993.activate(e,tp,eg,ep,ev,re,r,rp)
 		e3:SetLabelObject(tc)
 		e3:SetCondition(c81384993.rmcon1)
 		e3:SetOperation(c81384993.rmop1)
+		-- 注册在结束阶段将该怪兽除外的全局效果
 		Duel.RegisterEffect(e3,tp)
 	end
+	-- 完成特殊召唤的流程
 	Duel.SpecialSummonComplete()
 end
+-- 检查特殊召唤的怪兽是否仍在场且标记未改变，以决定是否在结束阶段除外
 function c81384993.rmcon1(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	if tc:GetFlagEffectLabel(81384993)~=e:GetLabel() then
@@ -59,6 +78,8 @@ function c81384993.rmcon1(e,tp,eg,ep,ev,re,r,rp)
 		return false
 	else return true end
 end
+-- 结束阶段除外该怪兽的具体操作
 function c81384993.rmop1(e,tp,eg,ep,ev,re,r,rp)
+	-- 将特殊召唤的怪兽表侧表示除外
 	Duel.Remove(e:GetLabelObject(),POS_FACEUP,REASON_EFFECT)
 end
--- c81476402.lua
+++ c81476402.lua
@@ -1,9 +1,14 @@
 --邪悪龍エビルナイト・ドラゴン
+-- 效果：
+-- 这个卡名的①的效果1回合只能使用1次。
+-- ①：把自己的手卡·场上1只暗属性怪兽解放才能发动。这张卡从手卡特殊召唤。
+-- ②：这张卡被战斗破坏时，以对方场上1只怪兽为对象才能发动。那只怪兽送去墓地。那之后，可以把这张卡特殊召唤。
 local s,id,o=GetID()
+-- 初始化函数，注册卡片的①和②效果
 function s.initial_effect(c)
-	--spsummon
+	-- ①：把自己的手卡·场上1只暗属性怪兽解放才能发动。这张卡从手卡特殊召唤。
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(id,0))  --"特殊召唤"
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
@@ -12,9 +17,9 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--to grave
+	-- ②：这张卡被战斗破坏时，以对方场上1只怪兽为对象才能发动。那只怪兽送去墓地。那之后，可以把这张卡特殊召唤。
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,1))  --"送去墓地"
 	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_SPSUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -23,31 +28,47 @@ function s.initial_effect(c)
 	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
 end
+-- 过滤可解放的暗属性怪兽的条件函数
 function s.rfilter(c,tp)
+	-- 检查解放该卡后是否能空出怪兽区域，且该卡是暗属性，且在场上表侧表示或在手卡
 	return Duel.GetMZoneCount(tp,c)>0 and c:IsAttribute(ATTRIBUTE_DARK) and (c:IsFaceup() or c:IsControler(tp))
 end
+-- ①效果的发动代价（Cost）函数，执行解放1只暗属性怪兽的操作
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
+	-- 检查玩家手卡或场上是否存在至少1只满足过滤条件的可解放怪兽作为发动代价
 	if chk==0 then return Duel.CheckReleaseGroupEx(tp,s.rfilter,1,REASON_COST,true,c,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	-- 给玩家发送提示信息，要求选择要解放的卡片
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)  --"请选择要解放的卡"
+	-- 让玩家选择1只满足过滤条件的可解放怪兽
 	local g=Duel.SelectReleaseGroupEx(tp,s.rfilter,1,1,REASON_COST,true,c,tp)
+	-- 解放选中的怪兽
 	Duel.Release(g,REASON_COST)
 end
+-- ①效果的发动准备（Target）函数，检查自身是否能特殊召唤并设置操作信息
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	-- 设置连锁处理的操作信息，表示此效果包含特殊召唤自身的操作
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
+-- ①效果的效果处理（Operation）函数，将自身特殊召唤
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
+		-- 将这张卡以表侧表示特殊召唤到发动玩家的场上
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
+-- ②效果的发动准备（Target）函数，选择对方场上1只怪兽作为对象，并根据自身位置动态调整效果分类
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() end
+	-- 检查对方场上是否存在可以作为效果对象的怪兽
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	-- 给玩家发送提示信息，要求选择要送去墓地的卡片
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)  --"请选择要送去墓地的卡"
+	-- 让玩家选择对方场上1只怪兽作为效果对象
 	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_MZONE,1,1,nil)
+	-- 设置连锁处理的操作信息，表示此效果包含将选中的对象送去墓地的操作
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
 	if e:GetHandler():IsLocation(LOCATION_GRAVE) then
 		e:SetCategory(CATEGORY_TOGRAVE+CATEGORY_GRAVE_SPSUMMON+CATEGORY_SPECIAL_SUMMON)
@@ -55,14 +76,22 @@ function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		e:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
 	end
 end
+-- ②效果的效果处理（Operation）函数，将对象怪兽送去墓地，并可选择是否将自身特殊召唤
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	-- 获取当前连锁中被选为效果对象的怪兽
 	local tc=Duel.GetFirstTarget()
+	-- 检查对象怪兽是否仍与效果相关，并将其因效果送去墓地，且确认其成功到达墓地
 	if tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_GRAVE)
+		-- 检查自身是否仍与效果相关，且不受王家长眠之谷的影响
 		and c:IsRelateToEffect(e) and aux.NecroValleyFilter()(c)
+		-- 检查玩家场上是否有空余的怪兽区域，且自身是否可以特殊召唤
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		-- 询问玩家是否选择将这张卡特殊召唤
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then  --"是否特殊召唤？"
+		-- 中断当前效果处理，使后续的特殊召唤处理与前面的送去墓地处理不视为同时进行
 		Duel.BreakEffect()
+		-- 将这张卡以表侧表示特殊召唤到发动玩家的场上
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
