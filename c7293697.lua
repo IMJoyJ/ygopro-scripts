f4cc559ca4af9b740fe63a6a02eb9513bf26bdc8
JoyJ <316351048@qq.com>
2026-08-12 01:22:52 +0800

    chore(scripts): added comments for cards c7293697, c78679226

--- c7293697.lua
+++ c7293697.lua
@@ -1,16 +1,22 @@
 --完全なる世界 トゥーン・ワールド
+-- 效果：
+-- 这个卡名的②的效果1回合可以使用最多3次。
+-- ①：这张卡只要在场地区域存在，卡名当作「卡通世界」使用。
+-- ②：1回合1次，自己主要阶段才能发动。把1张「卡通」卡或者有那卡名记述的卡从卡组加入手卡。
+-- ③：其他卡发动的效果适用之际，可以把自己场上1只卡通怪兽直到那个效果处理后除外（这个回合，这个卡名的这个效果不能把原本卡名相同的怪兽除外）。
 local s,id,o=GetID()
+-- 完全なる世界 トゥーン・ワールド
 function s.initial_effect(c)
-	--change code
+	-- 这张卡只要在场地区域存在，卡名当作「卡通世界」使用
 	aux.EnableChangeCode(c,15259703,LOCATION_FZONE)
-	--activate
+	-- 永续魔陷/场地卡通用的“允许发动”空效果，无此效果则无法发动
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--tohand
+	-- ②：1回合1次，自己主要阶段才能发动。把1张「卡通」卡或者有那卡名记述的卡从卡组加入手卡。
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,1))  --"检索"
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_FZONE)
@@ -18,7 +24,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	--remove
+	-- ③：其他卡发动的效果适用之际，可以把自己场上1只卡通怪兽直到那个效果处理后除外（这个回合，这个卡名的这个效果不能把原本卡名相同的怪兽除外）。
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetRange(LOCATION_FZONE)
@@ -28,40 +34,62 @@ function s.initial_effect(c)
 	e3:SetOperation(s.rmop)
 	c:RegisterEffect(e3)
 end
+-- 筛选「卡通」卡或有其记述的卡的过滤函数
 function s.thfilter(c)
+	-- 检查是否为「卡通」卡、或记述了「卡通」系列、或记述了「卡通世界」
 	return (c:IsSetCard(0x62) or aux.IsSetNameMonsterListed(c,0x62) or aux.IsCodeListed(c,15259703))
 		and c:IsAbleToHand()
 end
+-- 效果②的发动条件与标记检测
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 检查卡组是否存在可检索卡且本回合发动次数小于3次
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and Duel.GetFlagEffect(tp,id)<3 end
+	-- 注册效果②在本回合的使用次数标记
 	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	-- 设置操作信息：从卡组将1张卡加入手牌
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
+-- 效果②的处理：从卡组检索卡片并加入手牌
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	-- 提示玩家选择要加入手牌的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)  --"请选择要加入手牌的卡"
+	-- 让玩家选择1张符合条件的卡
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
+		-- 把选择的卡加入手牌
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		-- 向对方确认加入手牌的卡
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
+-- 效果③的触发条件：其他卡发动的效果适用之际且场上有卡通怪兽
 function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
 	return re:GetHandler()~=e:GetHandler()
+		-- 检查场上是否存在可以除外的卡通怪兽
 		and Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_MZONE,0,1,nil)
 end
+-- 筛选场上表侧表示且可除外的卡通怪兽
 function s.rmfilter(c)
 	return c:IsFaceup() and c:IsAbleToRemove() and c:IsType(TYPE_TOON)
 end
+-- 效果③的处理：选择并暂时除外1只卡通怪兽，效果处理后返回场上，并限制本回合同名怪兽不能再以此效果除外
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.SelectEffectYesNo(tp,e:GetHandler(),aux.Stringid(id,3)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	-- 询问玩家是否发动效果将怪兽除外
+	if Duel.SelectEffectYesNo(tp,e:GetHandler(),aux.Stringid(id,3)) then  --"是否把怪兽除外？"
+		-- 提示玩家选择要除外的卡
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)  --"请选择要除外的卡"
+		-- 让玩家选择1只符合条件的卡通怪兽
 		local tg=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_MZONE,0,1,1,nil)
+		-- 显示卡片发动提示
 		Duel.Hint(HINT_CARD,0,id)
+		-- 显示选择的目标怪兽
 		Duel.HintSelection(tg)
 		local rc=tg:GetFirst()
+		-- 将目标怪兽暂时除外
 		if Duel.Remove(rc,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
 			rc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+			-- 直到那个效果处理后除外
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 			e1:SetCode(EVENT_CHAIN_SOLVED)
@@ -70,7 +98,9 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetCountLimit(1)
 			e1:SetCondition(s.retcon)
 			e1:SetOperation(s.retop)
+			-- 注册效果处理完后让怪兽返回场上的延迟效果
 			Duel.RegisterEffect(e1,tp)
+			-- （这个回合，这个卡名的这个效果不能把原本卡名相同的怪兽除外）。
 			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_FIELD)
 			e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -79,16 +109,21 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 			e2:SetTarget(s.rmlimit)
 			e2:SetLabel(rc:GetOriginalCode())
 			e2:SetReset(RESET_PHASE+PHASE_END)
+			-- 注册限制本回合同原本卡名怪兽不能再被除外的玩家效果
 			Duel.RegisterEffect(e2,tp)
 		end
 	end
 end
+-- 限制该原本卡名的怪兽本回合不能再被此效果除外
 function s.rmlimit(e,c,tp,r,re)
 	return c:GetOriginalCode()==e:GetLabel() and re and re:GetLabel()==id and r&REASON_EFFECT~=0
 end
+-- 检查被除外怪兽的标记效果是否存在
 function s.retcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetLabelObject():GetFlagEffect(id)~=0
 end
+-- 将暂时除外的怪兽返回场上
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	-- 把被除外的怪兽以原本表示形式返回场上
 	Duel.ReturnToField(e:GetLabelObject())
 end
--- c78679226.lua
+++ c78679226.lua
@@ -1,8 +1,13 @@
 --未来への沈黙
+-- 效果：
+-- 这个卡名的卡在1回合只能发动1张。
+-- ①：从卡组把有「光之黄金柜」的卡名记述的1只怪兽加入手卡。自己场上有着「光之黄金柜」以及有那个卡名记述的怪兽存在的状态，把这张卡在自己·对方的战斗阶段发动的场合，再让双方各自直到手卡变成6张为止抽卡。
 local s,id,o=GetID()
+-- 对未来的沉默
 function s.initial_effect(c)
+	-- 注册「光之黄金柜」的卡名关联
 	aux.AddCodeList(c,79791878)
-	--Activate without draw
+	-- ①：从卡组把有「光之黄金柜」的卡名记述的1只怪兽加入手卡。自己场上有着「光之黄金柜」以及有那个卡名记述的怪兽存在的状态，把这张卡在自己·对方的战斗阶段发动的场合，再让双方各自直到手卡变成6张为止抽卡。
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,7 +17,6 @@ function s.initial_effect(c)
 	e1:SetTarget(s.ndtarget)
 	e1:SetOperation(s.ndactivate)
 	c:RegisterEffect(e1)
-	--Activate without draw
 	local e2=e1:Clone()
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DRAW)
 	e2:SetCondition(s.dcon)
@@ -20,56 +24,92 @@ function s.initial_effect(c)
 	e2:SetOperation(s.dactivate)
 	c:RegisterEffect(e2)
 end
+-- 筛选自己场上表侧表示「光之黄金柜」的过滤函数
 function s.sfilter(c)
 	return c:IsFaceup() and c:IsCode(79791878)
 end
+-- 筛选自己场上表侧表示记述了「光之黄金柜」的怪兽的过滤函数
 function s.mfilter(c)
+	-- 检查卡片是否表侧表示、记述了「光之黄金柜」且为怪兽卡
 	return c:IsFaceup() and aux.IsCodeListed(c,79791878) and c:IsType(TYPE_MONSTER)
 end
+-- 未满足额外抽卡条件时的发动条件
 function s.ndcon(e,tp,eg,ep,ev,re,r,rp)
 	return not s.dcon(e,tp,eg,ep,ev,re,r,rp)
 end
+-- 满足额外抽卡条件时的发动条件：战斗阶段且自己场上有「光之黄金柜」和记述了「光之黄金柜」的怪兽
 function s.dcon(e,tp,eg,ep,ev,re,r,rp)
+	-- 检查当前是否处于战斗阶段
 	return (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE)
+	-- 检查自己场上是否同时存在「光之黄金柜」及记述了「光之黄金柜」的怪兽
 	and Duel.IsExistingMatchingCard(s.sfilter,tp,LOCATION_ONFIELD,0,1,nil) and Duel.IsExistingMatchingCard(s.mfilter,tp,LOCATION_MZONE,0,1,nil)
 end
+-- 筛选卡组中记述了「光之黄金柜」的怪兽的过滤函数
 function s.filter(c)
+	-- 检查卡片是否记述了「光之黄金柜」、是否为怪兽卡且能否加入手牌
 	return aux.IsCodeListed(c,79791878) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
+-- 普通发动的目标选择与操作信息设置
 function s.ndtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 检查卡组是否存在可以加入手牌的卡
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	-- 设置操作信息：从卡组将1张卡加入手牌
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
+-- 包含抽卡效果发动的目标选择与操作信息设置
 function s.dtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 计算自己需要抽卡的张数（直到手牌变成6张，考虑发动时消耗此卡）
 	local ct1=5-Duel.GetMatchingGroupCount(nil,tp,LOCATION_HAND,0,e:GetHandler())
+	-- 计算对方需要抽卡的张数（直到手牌变成6张）
 	local ct2=6-Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)
+	-- 检查卡组是否存在可以检索加入手牌的卡
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
+		-- 检查自己是否有抽卡空间且能否抽卡
 		and ct1>0 and Duel.IsPlayerCanDraw(tp,ct1+1)
+		-- 检查对方是否有抽卡空间且能否抽卡
 		and ct2>0 and Duel.IsPlayerCanDraw(1-tp,ct2)
 	end
+	-- 设置操作信息：从卡组将1张卡加入手牌
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	-- 设置操作信息：自己抽卡
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct1)
+	-- 设置操作信息：对方抽卡
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,ct2)
 end
+-- 普通发动的效果处理：从卡组检索怪兽加入手牌
 function s.ndactivate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	-- 提示玩家选择要加入手牌的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)  --"请选择要加入手牌的卡"
+	-- 让玩家选择1张符合条件的卡
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
+		-- 把选择的卡加入手牌
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		-- 向对方确认加入手牌的卡
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
+-- 包含抽卡效果的发动处理：检索怪兽加入手牌后双方抽卡
 function s.dactivate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	-- 提示玩家选择要加入手牌的卡
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)  --"请选择要加入手牌的卡"
+	-- 让玩家选择1张符合条件的卡
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
+		-- 把选择的卡加入手牌
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		-- 向对方确认加入手牌的卡
 		Duel.ConfirmCards(1-tp,g)
 		if g:IsExists(Card.IsLocation,1,nil,LOCATION_HAND) then
+			-- 计算自己需要抽卡的数量（直到手牌为6张）
 			local ct1=6-Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
+			-- 计算对方需要抽卡的数量（直到手牌为6张）
 			local ct2=6-Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)
+			-- 若有玩家需要抽卡则中断当前效果处理
 			if ct1>0 or ct2>0 then Duel.BreakEffect() end
+			-- 自己抽卡直到手牌变成6张
 			if ct1>0 then Duel.Draw(tp,ct1,REASON_EFFECT) end
+			-- 对方抽卡直到手牌变成6张
 			if ct2>0 then Duel.Draw(1-tp,ct2,REASON_EFFECT) end
 		end
 	end
